711f70676c9518f950834d589aa732854939738e
NazmulHamim12 <149864562+NazmulHamim12@users.noreply.github.com>
2023-11-04 13:33:59 +0600

    Add files via upload

---
+++ java training/hami.js
@@ -0,0 +1,5 @@
+const shop = ["BUET","CUET","RUET"];
+
+document.getElementById('hami').innerHTML = shop[0];
+document.getElementById('hami2').innerHTML = shop[1];
+document.getElementById('hami3').innerHTML = shop[2];
